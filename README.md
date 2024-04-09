85799db45c0d43b5f26e9702043c6ea4b63ea4cd
dev-csr <srikanthchilukuri01@gmail.com>
2024-04-09 15:49:24 +0530

    first-commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Guess the number
+
+In background a random number is generated and you have to give your guess.
+
+To start the app use
+npm start
+
+add if there are any errors try
+npm install or npm update and then npm start
---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess === number) {
+    document.querySelector('.number').textContent = number;
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess > number ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+//
